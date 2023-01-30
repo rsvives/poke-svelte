12ceda6bed7d395287c73e71a8c2fc07df20d2d4
rodri-afa <rodrigo.afaformacion@gmail.com>
2023-01-30 19:49:45 +0100

    random pokemon

--- src/App.svelte
+++ src/App.svelte
@@ -1,50 +1,53 @@
 <script>
-	let name = 'afa students'
+  let name = "afa students";
 
-	let n1 = 9;
-	let n2 = 7;	
+  let n1 = 9;
+  let n2 = 7;
 
-	let listaCompra = ['leche','pan','patatas', 'Papel de aluminio', 'satélites'];
+  let listaCompra = [
+    "leche",
+    "pan",
+    "patatas",
+    "Papel de aluminio",
+    "satélites",
+  ];
 
-	let listaCompraFruta = [
-								{
-									nombre:'fresas',
-									emoji: '🍓',
-									cantidad:'1Kg'
-								},
-								{	nombre:'kiwis',
-									emoji: '🥝',
-									cantidad:'4uds'
-								},
-								{	nombre:'naranjas', 
-									emoji:'🍊',
-									cantidad:'una malla'
-								},
-							]
+  let listaCompraFruta = [
+    {
+      nombre: "fresas",
+      emoji: "🍓",
+      cantidad: "1Kg",
+    },
+    { nombre: "kiwis", emoji: "🥝", cantidad: "4uds" },
+    { nombre: "naranjas", emoji: "🍊", cantidad: "una malla" },
+  ];
 
-	const URL = 'https://pokeapi.co/api/v2/pokemon/448'		
+  let range = { min: 1, max: 900 };
+  let delta = range.max - range.min;
 
-	const fetchData = (async () => {
-		const response = await fetch(URL)
-		const data = await response.json()
-		return data
-	})()
+  let pokemon = 448;
+  //   let pokemon = Math.round(range.min + Math.random() * delta);
+  const URL = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+  console.log(pokemon);
 
-
+  const fetchData = (async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    return data;
+  })();
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<!-- <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
+  <h1>Hello {name}!</h1>
+  <!-- <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
 
-	{#if n1>n2}
-		<p>El primer número es mayor que el segundo</p>
-	{:else}
-		<p>El primer número es menor que el segundo</p>
-	{/if}
+  {#if n1 > n2}
+    <p>El primer número es mayor que el segundo</p>
+  {:else}
+    <p>El primer número es menor que el segundo</p>
+  {/if}
 
-
-	<!-- <h2>mi lista de la compra:</h2>
+  <!-- <h2>mi lista de la compra:</h2>
 
 	<ul>
 
@@ -53,101 +56,95 @@
 		{/each}
 
 	</ul> -->
-	<!-- <h2>mi lista de las frutas:</h2> -->
+  <!-- <h2>mi lista de las frutas:</h2> -->
 
-	<!-- <ul>
+  <!-- <ul>
 		{#each listaCompraFruta as fruta}
 			<li>{fruta.emoji} {fruta.nombre} x {fruta.cantidad}</li>
 		{/each}
 	</ul> -->
-
-	
-
 </main>
 <div id="pokemon">
-	{#await fetchData}
-		<p>...cargando</p>
-	{:then data}
-		<div class="card">
-			<div class="img-container">
-				<img src="{data.sprites.front_default}" alt="imagen de {data.name}">
-			</div>
-			<div class="text">
-
-				<h3>{data.name}</h3>
-				<ul>
-					<li>Altura: {data.height}</li>
-					<li>Peso: {data.weight}</li>
-				</ul>
-			</div>
-		</div>
-	{:catch error}
-		<p>Error:{error}</p>
-	{/await}
+  {#await fetchData}
+    <p>...cargando</p>
+  {:then data}
+    <div class="card">
+      <div class="img-container">
+        <img src={data.sprites.front_default} alt="imagen de {data.name}" />
+      </div>
+      <div class="text">
+        <h3>{data.name}</h3>
+        <ul>
+          <li>Altura: {data.height}</li>
+          <li>Peso: {data.weight}</li>
+        </ul>
+      </div>
+    </div>
+  {:catch error}
+    <p>Error:{error}</p>
+  {/await}
 </div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-	ul{
-		text-align: left;
-		max-width: 200px;
-		margin: auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+  ul {
+    text-align: left;
+    max-width: 200px;
+    margin: auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 
-	#pokemon{
-		max-width: 800px;
-		margin: auto;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-	}
-	#pokemon .card{
-		display: flex;
-		min-width: 300px;
-		box-shadow: 0 0 6px rgba(0, 0, 0, 0.3);
-		 border-radius: 12px;
-		 padding: 12px;
-		 gap: 12px;
-	}
-	#pokemon h3{
-		text-transform: capitalize;
-		margin-bottom: 12px;
-	}
-	#pokemon .img-container{
-		width:80px;
-		border: 1px solid gray;
-		padding: 8px;
-		border-radius: 12px;
-		box-sizing: border-box;
-		text-align: center;
-		display: flex;
-		justify-content: center;
-	
-	}
-	#pokemon img{
-		margin: auto;
-	}
+  #pokemon {
+    max-width: 800px;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  #pokemon .card {
+    display: flex;
+    min-width: 300px;
+    box-shadow: 0 0 6px rgba(0, 0, 0, 0.3);
+    border-radius: 12px;
+    padding: 12px;
+    gap: 12px;
+  }
+  #pokemon h3 {
+    text-transform: capitalize;
+    margin-bottom: 12px;
+  }
+  #pokemon .img-container {
+    width: 80px;
+    border: 1px solid gray;
+    padding: 8px;
+    border-radius: 12px;
+    box-sizing: border-box;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+  }
+  #pokemon img {
+    margin: auto;
+  }
 
-	#pokemon ul{
-		padding-left: 12px;
-	}
-
+  #pokemon ul {
+    padding-left: 12px;
+  }
 </style>
